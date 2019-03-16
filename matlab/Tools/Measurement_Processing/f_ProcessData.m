77aea6449fc28bef63e5af9a4f2d74dcdba2e8cd
saples <saples@hotmail.com>
2019-03-16 16:47:55 -0500

    MATLAB changes

--- matlab/Tools/Measurement_Processing/f_ProcessData.m
+++ matlab/Tools/Measurement_Processing/f_ProcessData.m
@@ -1,4 +1,4 @@
-[A] = f_ProcessData(imgfullpath,savetype,pathSep,dataformat,cameraPlane,n,PP,M,f,ProcessedDir)
+function [A] = f_ProcessData(imgfullpath,savetype,pathSep,dataformat,cameraPlane,n,PP,M,f,ProcessedDir,wait,infoDelim)
 %% Post-processing of the data (application of the metric of the degree of
 %%% extintion)
 
@@ -8,12 +8,12 @@ pause(wait) % Seconds before measuring as a safety measurement
 t1_dt = datetime; % store time
 disp('Processing started:'); disp(t1_dt)
 processedImgname = strcat(ProcessedDir,pathSep,'processed',infoDelim, ...
-  cameraPlane,infoDelim);
+                          cameraPlane,infoDelim);
 
 % for idxgral = 1:totalImgs
 %% Loading
 % load(directory+filename,variables)
-
+MeasInfo = []; % Variable initialization
 A = load(imgfullpath,'expImgs'); % imgfullpath comes from AutomatMeasure
 
 %% Cartesian coordinates with the lambda/D scaling
@@ -27,19 +27,19 @@ yang = f_scalePix2DiffAng(ypix,n,PP,M,f);
 switch metricSel
   case 1
     tit = 'Encircled Energy Factor metric';
-    [energy,radialIntensity] = f_calculateEEF(angle(mask),n,PP,M,f,shiftCart,metricProfile,tit);
+    [energy,radialIntensity] = f_calculateEEF(angle(mask),n,PP,M,f, ...
+                                              shiftCart,metricProfile,tit);
 end
 
 %% Saving
 processedImgfullpath = strcat(processedImgname,MeasInfo{idxgral});
-%  if savetype == 1 % .mat format
-%   % save(directory+filename,variables)
-%   save(processedImgfullpath,'A'); % .mat
-%  else % savetype = 2. dataformat is used
-%   % imwrite(variables,directory+filename+extension)
-% imwrite(expImgs{idxgral}, strcat(processedImgfullpath,dataformat));
-%  end
-%end
+if savetype == 1 % .mat format
+    % save(directory+filename,variables)
+    save(processedImgfullpath,'A'); % .mat
+else % savetype = 2. dataformat is used
+    % imwrite(variables,directory+filename+extension)
+    imwrite(expImgs{idxgral}, strcat(processedImgfullpath,dataformat));
+end
 
 %% End of the processing
 % Author: PhD student Jens de Pelsmaeker VUB B-PHOT 2018, Brussels, Belgium
